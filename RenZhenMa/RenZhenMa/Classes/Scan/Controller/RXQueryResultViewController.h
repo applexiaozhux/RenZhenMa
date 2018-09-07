5f1ba48d429cc0517aeb46ce369fffdaa115142e
li_ruixing <690734540@qq.com>
2018-09-07 09:07:19 +0800

    所用到的控制器已全部创建完成
    所用到的控制器已全部创建完成

---
+++ RenZhenMa/RenZhenMa/Classes/Scan/Controller/RXQueryResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  RXQueryResultViewController.h
+//  RenZhenMa
+//
+//  Created by 乔喜洋 on 2018/9/7.
+//  Copyright © 2018年 李瑞星. All rights reserved.
+// 产品查询结果
+
+#import <UIKit/UIKit.h>
+
+@interface RXQueryResultViewController : UIViewController
+
+@end
